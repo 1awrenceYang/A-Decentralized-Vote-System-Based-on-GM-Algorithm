52e3417203c0cb872b07fdba384c179363397743
1awrenceYang <73065631+1awrenceYang@users.noreply.github.com>
2022-10-10 21:15:50 +0800

    Update main.cpp
    backup

--- main.cpp
+++ main.cpp
@@ -50,117 +50,154 @@ int main()
     time(&seed);
     irand((unsigned long long)seed);
     
-    int plaintext = 1;
-    int cb = 0;
-    mip->IOBASE = HexIOBASAE;
-    big sk, k, a, b, p, g_x, g_y, c1_x_big, c1_y_big, c2_x_big, c2_y_big;
-    epoint* pk, * G, * c1, * c2;
-    uint32_t* c3;
-    uint8_t* c1_x, *c1_y, *c2_x, *c2_y;
-    sk = mirvar(0);
-    k = mirvar(0);
-    a = mirvar(0);
-    b = mirvar(0);
-    p = mirvar(0);
-    g_x = mirvar(0);
-    g_y = mirvar(0);
-    k = mirvar(0);
-    //kout = mirvar(0);
-    c1_x_big = mirvar(0);
-    c1_y_big = mirvar(0);
-    c2_x_big = mirvar(0);
-    c2_y_big = mirvar(0);
-    bigbits(256, sk);
-    //cotnum(sk,stdout);
-    bytes_to_big(32, Sm2CurveParamG_x, g_x);
-    bytes_to_big(32, Sm2CurveParamG_y, g_y);
-    bytes_to_big(32, Sm2CurveParam_a, a);
-    bytes_to_big(32, Sm2CurveParam_b, b);
-    bytes_to_big(32, Sm2CurveParamPrime, p);
-    G = epoint_init();
-    pk = epoint_init();
-    c1 = epoint_init();
-    c2 = epoint_init();
-    c3 = (uint32_t*)malloc(8 * sizeof(uint32_t));
-    c1_x = (uint8_t*)malloc(32 * sizeof(uint8_t));
-    c1_y = (uint8_t*)malloc(32 * sizeof(uint8_t));
-    c2_x = (uint8_t*)malloc(32 * sizeof(uint8_t));
-    c2_y = (uint8_t*)malloc(32 * sizeof(uint8_t));
-    ecurve_init(a, b, p, MR_BEST);
-    epoint_set(g_x, g_y, cb, G);
-    ecurve_mult(sk, G, pk);
-    char* k1 = (char*)malloc(32 * sizeof(char));
-    //cotnum(sk, stdout);
-    int m = 1;
-    int m1 = 1;
-    Encryption(m, pk, G, c1, c2, c3);
-    //big kk = mirvar(0);
-    //bytes_to_big(32, k1, kk);
-    
-    printf("-------------------------First Encryption Complete-----------------------------\n");
-    printf("C1 ciphertext:\n");
-    epoint_print(c1);
-    printf("C2 ciphertext:\n");
-    epoint_print(c2);
-    printf("C3 ciphertext:\n");
-    print_hash(c3);
-    printf("plaintext:\n");
-    printf("%d\n\n\n", m);
+    //int plaintext = 1;
+    //int cb = 0;
+    //mip->IOBASE = HexIOBASAE;
+    //big sk, k, a, b, p, g_x, g_y, c1_x_big, c1_y_big, c2_x_big, c2_y_big;
+    //epoint* pk, * G, * c1, * c2;
+    //uint32_t* c3;
+    //uint8_t* c1_x, *c1_y, *c2_x, *c2_y;
+    //sk = mirvar(0);
+    //k = mirvar(0);
+    //a = mirvar(0);
+    //b = mirvar(0);
+    //p = mirvar(0);
+    //g_x = mirvar(0);
+    //g_y = mirvar(0);
+    //k = mirvar(0);
+    ////kout = mirvar(0);
+    //c1_x_big = mirvar(0);
+    //c1_y_big = mirvar(0);
+    //c2_x_big = mirvar(0);
+    //c2_y_big = mirvar(0);
+    //bigbits(256, sk);
+    ////cotnum(sk,stdout);
+    //bytes_to_big(32, Sm2CurveParamG_x, g_x);
+    //bytes_to_big(32, Sm2CurveParamG_y, g_y);
+    //bytes_to_big(32, Sm2CurveParam_a, a);
+    //bytes_to_big(32, Sm2CurveParam_b, b);
+    //bytes_to_big(32, Sm2CurveParamPrime, p);
+    //G = epoint_init();
+    //pk = epoint_init();
+    //c1 = epoint_init();
+    //c2 = epoint_init();
+    //c3 = (uint32_t*)malloc(8 * sizeof(uint32_t));
+    //c1_x = (uint8_t*)malloc(32 * sizeof(uint8_t));
+    //c1_y = (uint8_t*)malloc(32 * sizeof(uint8_t));
+    //c2_x = (uint8_t*)malloc(32 * sizeof(uint8_t));
+    //c2_y = (uint8_t*)malloc(32 * sizeof(uint8_t));
+    //ecurve_init(a, b, p, MR_BEST);
+    //epoint_set(g_x, g_y, cb, G);
+    //ecurve_mult(sk, G, pk);
+    //char* k1 = (char*)malloc(32 * sizeof(char));
+    ////cotnum(sk, stdout);
+    //int m = 1;
+    //int m1 = 1;
+    //Encryption(m, pk, G, c1, c2, c3);
+    ////big kk = mirvar(0);
+    ////bytes_to_big(32, k1, kk);
+    //
+    //printf("-------------------------First Encryption Complete-----------------------------\n");
+    //printf("C1 ciphertext:\n");
     //epoint_print(c1);
+    //printf("C2 ciphertext:\n");
     //epoint_print(c2);
+    //printf("C3 ciphertext:\n");
     //print_hash(c3);
-    int i = 0;
-    /*try
-    {
-        i = Decryption(c1, c2, G, c3, sk);
-        printf("plaintext:%d\n", i);
-        printf("-------------------------First Decryption Complete-----------------------------\n");
-        
-        
-    }
-    catch (int error)
-    {
-        PrintErrorMessage(error);
-    }*/
-    printf("\n\n\n");
-    
-    epoint* c11, * c22;
-    uint32_t* c33;
-    c11 = epoint_init();
-    c22 = epoint_init();
-    c33 = (uint32_t*)malloc(8 * sizeof(uint32_t));
-    Encryption(m1, pk, G, c11, c22, c33);
-    printf("-------------------------Second Encryption Complete-----------------------------\n");
-    printf("C1 ciphertext:\n");
-    epoint_print(c11);
-    printf("C2 ciphertext:\n");
-    epoint_print(c22);
-    printf("C3 ciphertext:\n");
-    print_hash(c33);
-    printf("plaintext:\n");
-    printf("%d\n\n\n", m1);
-    /*int m2=Decryption(c11, c22, G, c33, sk);
-    printf("Decryption plaintext result:\n");
-    printf("%d", m2);*/
-    epoint* HomoC1, * HomoC2;
-    HomoC1 = epoint_init();
-    HomoC2 = epoint_init();
-    HomoEncryption(c1, c11, c2, c22, HomoC1, HomoC2);
-    //HomoEncryption(HomoC1, c1, HomoC2, c2, HomoC1, HomoC2);
-    printf("Homo C1 Ciphertext:\n");
-    epoint_print(HomoC1);
-    printf("Homo C2 Ciphertext:\n");
-    epoint_print(HomoC2);
+    //printf("plaintext:\n");
+    //printf("%d\n\n\n", m);
+    ////epoint_print(c1);
+    ////epoint_print(c2);
+    ////print_hash(c3);
+    //int i = 0;
+    ///*try
+    //{
+    //    i = Decryption(c1, c2, G, c3, sk);
+    //    printf("plaintext:%d\n", i);
+    //    printf("-------------------------First Decryption Complete-----------------------------\n");
+    //    
+    //    
+    //}
+    //catch (int error)
+    //{
+    //    PrintErrorMessage(error);
+    //}*/
+    //printf("\n\n\n");
+    //
+    //epoint* c11, * c22;
+    //uint32_t* c33;
+    //c11 = epoint_init();
+    //c22 = epoint_init();
+    //c33 = (uint32_t*)malloc(8 * sizeof(uint32_t));
+    //Encryption(m1, pk, G, c11, c22, c33);
+    //printf("-------------------------Second Encryption Complete-----------------------------\n");
+    //printf("C1 ciphertext:\n");
+    //epoint_print(c11);
+    //printf("C2 ciphertext:\n");
+    //epoint_print(c22);
+    //printf("C3 ciphertext:\n");
+    //print_hash(c33);
+    //printf("plaintext:\n");
+    //printf("%d\n\n\n", m1);
+    ///*int m2=Decryption(c11, c22, G, c33, sk);
+    //printf("Decryption plaintext result:\n");
+    //printf("%d", m2);*/
+    //epoint* HomoC1, * HomoC2;
+    //HomoC1 = epoint_init();
+    //HomoC2 = epoint_init();
+    //HomoEncryption(c1, c11, c2, c22, HomoC1, HomoC2);
+    ////HomoEncryption(HomoC1, c1, HomoC2, c2, HomoC1, HomoC2);
+    //printf("Homo C1 Ciphertext:\n");
+    //epoint_print(HomoC1);
+    //printf("Homo C2 Ciphertext:\n");
+    //epoint_print(HomoC2);
 
-    try
+    //try
+    //{
+    //    int m2 = 0;
+    //    HomoDecryption(HomoC1, HomoC2, G, sk, &m2);
+    //    printf("HomoDecryption result:\n");
+    //    printf("%x", m2);
+    //}
+    //catch (int error)
+    //{
+    //    PrintErrorMessage(error);
+    //}
+    big test[3];
+    test[0] = mirvar(1);
+    test[1] = mirvar(3);
+    test[2] = mirvar(2);
+    big** aij = (big**)malloc(16 * sizeof(big));
+    aij[0] = CalFij(4, 3, test);
+    test[0] = mirvar(6);
+    test[1] = mirvar(2);
+    test[2] = mirvar(4);
+    aij[1] = CalFij(4, 3, test);
+    test[0] = mirvar(3);
+    test[1] = mirvar(1);
+    test[2] = mirvar(5);
+    aij[2] = CalFij(4, 3, test);
+    test[0] = mirvar(6);
+    test[1] = mirvar(3);
+    test[2] = mirvar(1);
+    aij[3] = CalFij(4, 3, test);
+
+    /*for (int i = 0; i < 4; i++)
     {
-        int m2 = 0;
-        HomoDecryption(HomoC1, HomoC2, G, sk, &m2);
-        printf("HomoDecryption result:\n");
-        printf("%x", m2);
-    }
-    catch (int error)
-    {
-        PrintErrorMessage(error);
-    }
+        for (int j = 0; j < 4; j++)
+            cotnum(aij[i][j], stdout);
+    }*/
+    big* result = CalSecretShareGiven(4, 3, aij);
+    cotnum(result[0], stdout);
+    cotnum(result[1], stdout);
+    cotnum(result[2], stdout);
+    cotnum(result[3], stdout);
+    big x = mirvar(3);
+    big y = mirvar(2);
+    big z = mirvar(0);
+    divide(x, y, z);
+    cotnum(x, stdout);
+    cotnum(y, stdout);
+    cotnum(z, stdout);
+
 }
